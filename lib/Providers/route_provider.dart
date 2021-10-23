f53c5df74d4a4ad2635a9aae55284f793c1e1d59
SexyPinata <marcus.a.nilsson95@gmail.com>
2021-10-23 16:29:45 +0200

    a

--- lib/Providers/route_provider.dart
+++ lib/Providers/route_provider.dart
@@ -1,25 +1,34 @@
+// @dart=2.9
 import 'package:budcomapp/Models/route_model.dart';
 import 'package:budcomapp/Services/database.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:uuid/uuid.dart';
 
 class RouteProvider with ChangeNotifier {
   final firestoreService = FirestoreService();
-  List<Object?>? _list_of_aps;
-  String? _name;
-  String? _id;
+  List<Object> _list_of_aps;
+  String _name;
+  String _id;
+  var uuid = const Uuid();
 //getters
   Stream<List<Route_Model>> get entries => firestoreService.getRoutes();
-  List<Object?>? get list_of_aps => _list_of_aps;
-  String? get name => _name;
+  Future<Route_Model> get entrie => firestoreService.getRoute(_id);
+  List<Object> get list_of_aps => _list_of_aps;
+  String get name => _name;
 
 //setters
-  set changeList_of_aps(List<Object?>? newList_of_aps) {
+  set changeList_of_aps(List<Object> newList_of_aps) {
     _list_of_aps = newList_of_aps;
     notifyListeners();
   }
 
-  set changeName(String? newName) {
+  set changeName(String newName) {
     _name = newName;
+    notifyListeners();
+  }
+
+  set setId(String value) {
+    _id = value;
     notifyListeners();
   }
 
@@ -40,13 +49,13 @@ class RouteProvider with ChangeNotifier {
     if (_id == null) {
       //Add
       var newEntry =
-          Route_Model(list_of_aps: _list_of_aps!, name: _name!, id: _id!);
+          Route_Model(list_of_aps: _list_of_aps, name: _name, id: uuid.v1());
       print(newEntry.name);
       firestoreService.setRoute(newEntry);
     } else {
       //Edit
       var updatedEntry =
-          Route_Model(list_of_aps: _list_of_aps!, name: _name!, id: _id!);
+          Route_Model(list_of_aps: _list_of_aps, name: _name, id: _id);
       firestoreService.setRoute(updatedEntry);
     }
   }
